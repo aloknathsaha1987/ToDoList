a5ddec3e9cee495b7ba1986fcf1920574a07c74f
aloknathsaha1987 <alok.saha@nyu.edu>
2015-02-17 21:26:09 -0500

    ListView Added to swipe

--- app/src/main/java/com/aloknath/notetakingapp/fragments/ScreenSlidePageFragment.java
+++ app/src/main/java/com/aloknath/notetakingapp/fragments/ScreenSlidePageFragment.java
@@ -1,10 +1,13 @@
 package com.aloknath.notetakingapp.fragments;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.aloknath.notetakingapp.R;
@@ -12,7 +15,7 @@ import com.aloknath.notetakingapp.R;
 /**
  * Created by ALOKNATH on 2/17/2015.
  */
-public class ScreenSlidePageFragment extends Fragment {
+public class ScreenSlidePageFragment extends ListFragment {
     /**
      * The argument key for the page number this fragment represents.
      */
@@ -22,6 +25,8 @@ public class ScreenSlidePageFragment extends Fragment {
      * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
      */
     private int mPageNumber;
+    private ListView dayTaskListView;
+    private ArrayAdapter<String> mAdapter;
 
     /**
      * Factory method for this fragment class. Constructs a new fragment for the given page number.
@@ -48,18 +53,29 @@ public class ScreenSlidePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout containing a title and body text.
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+
         ViewGroup rootView = (ViewGroup) inflater
                 .inflate(R.layout.fragment_screen_slide_page, container, false);
-
-        // Set the title view to show the page number.
-        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                getString(R.string.title_template_step, mPageNumber + 1));
 
         return rootView;
     }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Do something with the data
+    }
+
+
+
     /**
      * Returns the page number represented by this fragment object.
+     * java.lang.RuntimeException: Content has view with id attribute 'android.R.id.list' that is not a ListView class
      */
     public int getPageNumber() {
         return mPageNumber;
